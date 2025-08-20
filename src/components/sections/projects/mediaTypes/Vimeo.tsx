7bb9dfa5fdfe073ecd05e3fcb0b43cc1579f24af
Matthew Roosa <nasman98@yahoo.com>
2025-08-20 15:41:56 -0400

    Fix Vimeo embed

--- src/components/sections/projects/mediaTypes/Vimeo.tsx
+++ src/components/sections/projects/mediaTypes/Vimeo.tsx
@@ -3,16 +3,19 @@ import { type MediaItem } from "../Media";
 const Vimeo = ({item}:{item:MediaItem}) => {
 
     const content = item.content[0];
-    // strip out everything but the id
-
-
-
+    // Check for vimeo URL, return only the video ID
+    const domain = "vimeo.com/";
+    const hasURL = content.src.lastIndexOf(domain);
+    const vimeoId = content.src.substring(hasURL + domain.length, content.src.length - 1);
+    // split to array to check for private hash, form url as necessary
+    const vimeoIdAry = vimeoId.split("/");
+    const vimeoURL = (vimeoIdAry.length > 1) ? `${vimeoIdAry[0]}?h=${vimeoIdAry[1]}` : vimeoIdAry[0];
 
     // Get ratio for proper centering/fill (round 2 places)
     const ratio = (content.height && content.width) ? Math.round((content.height / content.width) * 10000) / 100 : 56.25;
     const padding = `${ratio}% 0 0 0`;
 
-    const contentURL = `https://player.vimeo.com/video/${content.src}&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;dnt=1`;
+    const contentURL = `https://player.vimeo.com/video/${vimeoURL}&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;dnt=1`;
     const hasShadow = (item.disableShadow) ? "no-shadow" : undefined;
     
 
